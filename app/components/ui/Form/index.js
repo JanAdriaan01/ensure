ad9ed93e726cb613c8dbc673a26296de2fe97902
JanAdriaan01 <janadriaan@lethabocabling.co.za>
2026-04-28 08:19:15 +0200

    v70

--- app/components/ui/Form/index.js
+++ app/components/ui/Form/index.js
@@ -1,8 +1,4 @@
-export { default as FormInput } from './FormInput';
-export { default as FormSelect } from './FormSelect';
-export { default as FormTextarea } from './FormTextarea';
-export { default as FormDatePicker } from './FormDatePicker';
-export { default as FormCurrencyInput } from './FormCurrencyInput';
+
 export { default as FormInput } from './FormInput';
 export { default as FormSelect } from './FormSelect';
 export { default as FormTextarea } from './FormTextarea';
